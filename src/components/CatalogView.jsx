3e93f4622fe066e76f99966eaccbcadd3d476e05
Agustin <agustingomez22059898@gmail.com>
2024-08-13 15:40:23 -0300

    "Modificacion de base de datos. Se anexo al backend con java"

--- src/components/CatalogView.jsx
+++ src/components/CatalogView.jsx
@@ -2,21 +2,28 @@ import { useEffect, useState } from "react";
 import { getProducts } from "../services/productServices";
 import { ProductCardView } from "./ProductCardView";
 
-export const CatalogView = ({handler}) => {
+export const CatalogView = ({ handler }) => {
 
     const [products, setProducts] = useState([]);
+    const [isLoading,setIsLoading] = useState(true);
+
+    const findAll = async () => {
+        const prods = await getProducts();
+        setProducts(prods);
+        setIsLoading(false);
+    }
 
     useEffect(
         () => {
-            setProducts(getProducts());
-            console.log(getProducts());
+            findAll()
         }, []
     );
 
     return (
         <>
+        { isLoading && <div className="alert alert-info">Cargando...</div>}
             <div className="row">
-                {products.map(product=> (
+                {products.map(product => (
                     <div className="col-4 my-2" key={product.id}>
                         <ProductCardView handler={handler} id={product.id} name={product.name} description={product.description} price={product.price} />
                     </div>
